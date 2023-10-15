ccdd0bea75c5f274149285ca915263ad89914bbf
Dustin Manuerhofer <90622168+dustinmauerhofer@users.noreply.github.com>
2023-10-15 22:56:41 +0200

    IT WORKS (I AM THE BEST)

--- deep learning network/main.py
+++ deep learning network/main.py
@@ -5,15 +5,12 @@ import cv2
 import imghdr
 
 from keras.models import Sequential
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization
-=======
-=======
->>>>>>> parent of c5c1f28 (dln works now)
+
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 from keras.saving.saving_api import load_model
->>>>>>> parent of c5c1f28 (dln works now)
+
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
@@ -57,61 +54,36 @@ test = data.skip(train_size + val_size).take(test_size)
 
 # Data Agumentation Layers
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 
 # Deep Learning
+model = Sequential();
 
-model = Sequential()
-
-model.add(Conv2D(64, (3, 3),1, activation='relu', input_shape=(256, 256, 3)))
+model.add(Conv2D(64, (3, 3), 1, activation='relu', input_shape=(256, 256, 3)))
 model.add(BatchNormalization())
 model.add(MaxPooling2D())
 
-model.add(Conv2D(128, (3, 3),1, activation='relu',))
+model.add(Conv2D(128, (3, 3), 1, activation='relu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D())
 
-model.add(Conv2D(64, (3, 3),1, activation='relu',))
+model.add(Conv2D(64, (3, 3), 1, activation='relu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D())
 
-model.add(Conv2D(64, (3, 3),1, activation='relu',))
+
+model.add(Conv2D(64, (3, 3), 1, activation='relu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D())
 
-model.add(Conv2D(64, (3, 3),1, activation='relu',))
+
+model.add(Conv2D(64, (3, 3), 1, activation='relu'))
 model.add(BatchNormalization())
-=======
-model = Sequential()
-
-model.add(Conv2D(16, (3, 3), 1, activation='relu', input_shape=(256, 256, 3)))
-=======
-model = Sequential()
-
-model.add(Conv2D(16, (3, 3), 1, activation='relu', input_shape=(256, 256, 3)))
 model.add(MaxPooling2D())
 
-model.add(Conv2D(32, (3, 3), 1, activation='relu'))
-model.add(MaxPooling2D())
-
-model.add(Conv2D(16, (3, 3), 1, activation='relu'))
->>>>>>> parent of c5c1f28 (dln works now)
-model.add(MaxPooling2D())
-
-model.add(Conv2D(32, (3, 3), 1, activation='relu'))
-model.add(MaxPooling2D())
-
-model.add(Conv2D(16, (3, 3), 1, activation='relu'))
->>>>>>> parent of c5c1f28 (dln works now)
-model.add(MaxPooling2D())
-
-model.add(Dense(num_classes, activation='softmax'))
-
+model.add(Flatten())
+model.add(Dense(5, activation='softmax'))
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model
 
 logdir = 'logs'
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
@@ -119,23 +91,12 @@ tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-#model.save("AugmentedV1.h5");
-=======
-=======
->>>>>>> parent of c5c1f28 (dln works now)
-model.save("apple_pear_orange_blueberry_testV2.h5");
-
-
-# test
 
 
 
-<<<<<<< HEAD
->>>>>>> parent of c5c1f28 (dln works now)
-=======
->>>>>>> parent of c5c1f28 (dln works now)
+#model.save("apple_pear_orange_blueberry_testV2.h5");
+
+
 
 # Evaluate Performance
 
