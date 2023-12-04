e575c27937e6c24df159749a0894afda2aafcee3
Dustin Manuerhofer <90622168+dustinmauerhofer@users.noreply.github.com>
2023-12-04 13:06:48 +0100

    google cloud setup works

--- DLNServer/main.py
+++ DLNServer/main.py
@@ -1,3 +1,4 @@
+import io
 import os
 import numpy as np
 import tensorflow as tf
@@ -7,43 +8,39 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Load the pre-trained model
-model_path = r"C:\Users\dustin\Desktop\augmentedV1.h5"
+model_path = r"augmentedV1.h5"
 loaded_model = tf.keras.models.load_model(model_path)
 
 # Define class labels
 class_labels = ["apple", "blueberry", "milk", "orange", "pear"]
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET','POST'])
 def predict():
-    try:
-        # Get the uploaded image file
-        image_file = request.files['image']
+   if request.method == 'POST':
+       file = request.files.get('file')
+       if file is None or file.filename == "":
+           return jsonify({"error": "no file"})
 
-        # Ensure the file is an image
-        if image_file and allowed_file(image_file.filename):
+       try:
+            image_bytes = file.read()
+            pillow_img = Image.open(io.BytesIO(image_bytes)).convert('L')
+
             # Read and preprocess the image
-            img = Image.open(image_file)
+            img = pillow_img.convert('RGB')  # Convert to RGB mode
             img = img.resize((256, 256))  # Resize the image to the desired size
             img = np.array(img) / 255.0  # Normalize the image to [0, 1]
-            img = np.expand_dims(img, axis=0)
+            img = np.expand_dims(img, axis=0)  # Add a batch dimension
+
 
             # Predict the class label
             predictions = loaded_model.predict(img)
             predicted_class = np.argmax(predictions, axis=1)
             predicted_label = class_labels[predicted_class[0]]
-
             return jsonify({'predicted_label': predicted_label})
-
-        else:
-            return jsonify({'error': 'Invalid file format. Please upload an image.'})
-    except Exception as e:
-        return jsonify({'error': 'An error occurred: {}'.format(str(e))})
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'jpg', 'jpeg', 'png'}
-
+       except Exception as e:
+            return jsonify({'error': 'An error occurred: {}'.format(str(e))})
+   return "OK"
 
 if __name__ == '__main__':
     app.run(debug=True)
