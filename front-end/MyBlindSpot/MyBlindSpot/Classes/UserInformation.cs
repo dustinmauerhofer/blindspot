a566e400a6c24c383039ade83b1c50e1913b542e
Dustin Manuerhofer <90622168+dustinmauerhofer@users.noreply.github.com>
2024-01-15 13:37:00 +0100

    added UserInfo Singelton

--- front-end/MyBlindSpot/MyBlindSpot/Classes/UserInformation.cs
+++ front-end/MyBlindSpot/MyBlindSpot/Classes/UserInformation.cs
@@ -1,18 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyBlindSpot.Classes
 {
     public class UserInformation
     {
-        public int Id { get; }
+        private static UserInformation instance;
 
-        public UserInformation(int id) 
+        // Properties
+        public int? Id { get; private set; }
+        public string UserName { get; private set; }
+
+        // Private constructor to prevent instantiation outside the class
+        private UserInformation()
         {
-            Id = id;
+            Id = null;
+            UserName = null;
+        }
+
+        // Method to get the singleton instance
+        public static UserInformation Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new UserInformation();
+                }
+                return instance;
+            }
+        }
+
+        // Method to set user information
+        public void SetUserInformation(int id, string userName)
+        {
+            if (Id == null)
+            {
+                Id = id;
+                UserName = userName;
+            }
+            else
+            {
+                // Handle the case where user information is already set
+                throw new InvalidOperationException("User information can only be set once.");
+            }
         }
     }
 }
