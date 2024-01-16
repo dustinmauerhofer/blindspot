a6c8bfbf25cf3210d1a038e0fa347b897677b8f4
Dustin Manuerhofer <90622168+dustinmauerhofer@users.noreply.github.com>
2024-01-16 11:33:56 +0100

    Revert "added UserInfo Singelton"
    This reverts commit a566e400a6c24c383039ade83b1c50e1913b542e.

--- front-end/MyBlindSpot/MyBlindSpot/Classes/UserInformation.cs
+++ front-end/MyBlindSpot/MyBlindSpot/Classes/UserInformation.cs
@@ -1,48 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MyBlindSpot.Classes
 {
     public class UserInformation
     {
-        private static UserInformation instance;
+        public int Id { get; }
 
-        // Properties
-        public int? Id { get; private set; }
-        public string UserName { get; private set; }
-
-        // Private constructor to prevent instantiation outside the class
-        private UserInformation()
+        public UserInformation(int id) 
         {
-            Id = null;
-            UserName = null;
-        }
-
-        // Method to get the singleton instance
-        public static UserInformation Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new UserInformation();
-                }
-                return instance;
-            }
-        }
-
-        // Method to set user information
-        public void SetUserInformation(int id, string userName)
-        {
-            if (Id == null)
-            {
-                Id = id;
-                UserName = userName;
-            }
-            else
-            {
-                // Handle the case where user information is already set
-                throw new InvalidOperationException("User information can only be set once.");
-            }
+            Id = id;
         }
     }
 }
