d43244f11677f32e8ca3b1e32b22cb153cf37277
Dustin Manuerhofer <90622168+dustinmauerhofer@users.noreply.github.com>
2024-01-17 10:23:36 +0100

    update Class Folder > old is now there

---
+++ front-end/MyBlindSpot/MyBlindSpot/Classes/RegisterArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlindSpot.Classes
+{
+    public class RegisterArgs : EventArgs
+    {
+        public string Response { get; }
+
+        public RegisterArgs(string response)
+        {
+            Response = response;
+        }
+    }
+}
